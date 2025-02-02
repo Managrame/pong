ea631dd6be111869c1ff0dcae327b07156a6e37a
julien_sotoca_ferrata <jx1001.29@gmail.com>
2025-02-02 22:14:31 +0100

    collision p2 règlée

--- pong.js
+++ pong.js
@@ -53,7 +53,7 @@ function update(){
     /* mouvement ball */
     if (
         (p1.y < ball.y + ball.height/4 && ball.y + ball.height*3/4 < p1.y + p1.height && ball.x <= p1.width) || // le joueur 1 a rattrapé la balle
-        (p2.y < ball.y + ball.height/4 && ball.y + ball.height*3/4 < p2.y + p2.height && ball.x >= o.width - p2.width) || // le joueur 2 a rattrapé la balle
+        (ball.x + ball.width >= p2.x && ball.x <= p2.x + p2.width && ball.y + ball.height >= p2.y && ball.y <= p2.y + p2.height) || // le joueur 2 a rattrapé la balle
         (ball.x <= 0 || ball.x >= o.width - ball.width) // la balle rebondit en haut ou en bas
     ){ball.vx *= -1;}
     else if (ball.y <= 0 || ball.y >= o.height - ball.height)
